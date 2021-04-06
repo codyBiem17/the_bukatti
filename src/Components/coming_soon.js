b2e05cc628614db1baf2e04ad44b018389a73ca8
biem <maryam.bello@tek-experts.com>
2021-04-06 15:29:24 +0000

    first commit

---
+++ src/Components/coming_soon.js
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+    return (
+        <div id="bgImage">
+            <div id="logo">the_bukattee</div>
+            <div id="bgText">
+                Coming Soon
+            </div>
+        </div>
+    )
+}
+export {LandingPage}
