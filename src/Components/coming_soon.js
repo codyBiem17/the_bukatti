05f79795c1e2880b18a1f38984374a8c95bb621c
biem <maryam.bello@tek-experts.com>
2021-04-07 15:43:54 +0000

    -fixing reactstarp styles

--- src/Components/coming_soon.js
+++ src/Components/coming_soon.js
@@ -1,11 +1,20 @@
+import React from 'react'
+import {Container, Row, Col} from "reactstrap"
+
 const LandingPage = () => {
     return (
-        <div id="bgImage">
-            <div id="logo">the_bukattee</div>
-            <div id="bgText">
-                Coming Soon
-            </div>
-        </div>
+        <Container id="bgImage">
+            <Row>
+                <Col xs="6"  id="logo">
+                    <p>the_bukattee</p>
+                </Col>
+                <Col id="bgText">
+                    <h1>Coming Soon</h1>
+                    <hr />
+                    <p>23 days left</p>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export {LandingPage}
