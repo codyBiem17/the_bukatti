dfac008b66523c4ac48ae8da762b20b92245208b
biemislam <bellomaryam@ymail.com>
2021-04-17 10:31:27 +0100

    working on sections on homepage

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -33,18 +33,45 @@ const HomePage = () => {
                         </Col>
                     </Row>
             </Container>
-            <Container className="container-2" fluid={true}>
-                <Row>
+            <Container className="container-2 containers" fluid={true}>
+                <Row className="row-1"> 
                     <Col xs="12">
-                        <img src={Waiter} />
+                        <p className="para-header">Now You Are Here!</p> 
                     </Col>
-                    <Col xs="12">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in mattis risus. Etiam auctor vehicula ex quis finibus. Nullam augue nulla, pharetra at tempor non, commodo a sem. Nullam eget sapien nunc. Suspendisse malesuada, ligula quis vestibulum porta, nisi tortor finibus nulla, sit amet consequat enim lectus quis neque. Maecenas sit amet lectus at magna accumsan semper. Aliquam sollicitudin volutpat erat ac blandit. Cras molestie accumsan dapibus. Aliquam lobortis porttitor interdum. In iaculis, libero eget tempor imperdiet, ipsum est iaculis lacus, a pulvinar velit lacus vitae mauris. Pellentesque lacinia felis nulla, malesuada rutrum enim tristique id.
-
-                        Nullam vel risus urna. Nullam accumsan eu augue placerat tempor. Fusce pulvinar et justo eget fermentum. Donec blandit luctus ante, at fermentum erat auctor sit amet. Curabitur vestibulum pretium tincidunt. Nam et hendrerit est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam pretium magna quam, ac fermentum orci volutpat ac. Ut vel sapien enim. Aliquam eros nisl, euismod ut nisl vel, laoreet fermentum leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec condimentum elit egestas ligula hendrerit, ut porttitor libero cursus. Nam viverra 
-                        ipsum et mi sollicitudin accumsan. Vestibulum sit amet diam a nibh ullamcorper gravida id id augue. Vestibulum semper est vulputate nibh dapibus, non dictum odio fermentum.
-                    </Col>
+                    <Row>
+                        <Col xs="12">
+                            <p className="welcome-caption">Welcome to Da_Bukattee...our tasty foods, way to your stomach</p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                            </p>
+                        </Col>
+                        <Col xs="12">
+                            <img src={Waiter} />
+                        </Col>
+                    </Row>
                 </Row>
+            </Container>
+            <Container className="containers container-3" fluid={true}>
+                <Row className="row-2"> 
+                    <Col xs="12">
+                        <p className="para-header">Our Services</p> 
+                    </Col>
+                    <Row>
+                        <Col xs="12">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                            </p>
+                        </Col>
+                        <Col xs="12">
+                            <img src={Waiter} />
+                        </Col>
+                    </Row>
+                </Row>
+                
             </Container>
         </>
     )
