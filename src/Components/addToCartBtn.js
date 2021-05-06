2e6da912040dab8722cd8821a04b8a911459456a
biem <maryam.bello@tek-experts.com>
2021-05-06 15:41:50 +0000

    btn battle

--- src/Components/addToCartBtn.js
+++ src/Components/addToCartBtn.js
@@ -1,27 +1,41 @@
 import React, { useState } from 'react'
 import {Button} from'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { NavLink } from 'react-router-dom'
+
 
 const AddToCartBtn = ({isClicked, setIsClicked}) =>{
-    // const [showBtnText, setShowBtn] = useState({text: 'Added to cart', icon: ''})
+    const [showBtnText, setShowBtn] = useState({text: 'Added to cart'})
+    const [showMessage, setShowMessage] = useState('')
 
     const handleAddToCart = (e) => {
+        setIsClicked(true)
 
-        // e.currentTarget.id ? setIsClicked(true) : null
+        const checkIcon = <FontAwesomeIcon icon="check" />
+        setShowBtn({text: 'Added', icon: checkIcon})
 
-    //     const checkIcon = <FontAwesomeIcon icon="check" />
-    //     setShowBtn({text: 'Added', icon: checkIcon})
+        setShowMessage('View Cart')
+
     // }
     // const setBtn = () =>{
         
         
     }
+    
 
 
     return (
-        <Button id="btn-cart" onClick={handleAddToCart} className="p-3">
-            { isClicked ? 'clicked' : 'click me'}
-        </Button>
+        <>
+            <Button id="btn-cart" onClick={handleAddToCart} className="p-3">
+                { isClicked ? 
+                    <span> {showBtnText.text} {showBtnText.icon} </span> : 
+                    showBtnText.text
+                }
+            </Button>
+            {
+                isClicked && <span> <NavLink to="#">{showMessage}</NavLink></span>
+            }
+        </>
     )
 
 }
