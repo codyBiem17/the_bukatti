358d5ab7ea0c38342f3d18cf5952be66b9560ff3
biem <maryam.bello@tek-experts.com>
2021-05-06 15:41:50 +0000

    btn battle

--- src/Components/addToCartBtn.js
+++ src/Components/addToCartBtn.js
@@ -3,24 +3,26 @@ import {Button} from'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const AddToCartBtn = ({isClicked, setIsClicked}) =>{
-    // const [showBtnText, setShowBtn] = useState({text: 'Added to cart', icon: ''})
+    const [showBtnText, setShowBtn] = useState({text: 'Added to cart', icon: ''})
 
     const handleAddToCart = (e) => {
+        setIsClicked(!isClicked)
 
         // e.currentTarget.id ? setIsClicked(true) : null
 
-    //     const checkIcon = <FontAwesomeIcon icon="check" />
-    //     setShowBtn({text: 'Added', icon: checkIcon})
+        const checkIcon = <FontAwesomeIcon icon="check" />
+        setShowBtn({text: 'Added', icon: checkIcon})
     // }
     // const setBtn = () =>{
         
         
     }
+    
 
 
     return (
         <Button id="btn-cart" onClick={handleAddToCart} className="p-3">
-            { isClicked ? 'clicked' : 'click me'}
+            { isClicked ? (showBtnText.text, showBtnText.icon) : showBtnText.text, showBtnText.icon}
         </Button>
     )
 
