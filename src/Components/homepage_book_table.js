4c7cdceea97cf50dae1a9796bf7edd470f6537db
biemislam <bellomaryam@ymail.com>
2021-05-29 10:22:40 +0100

    reservation animation

--- src/Components/homepage_book_table.js
+++ src/Components/homepage_book_table.js
@@ -4,7 +4,7 @@ import WaiterServing from '../assets/images/waiter-setting-table.jpg'
 import { 
     Button, Row, Col, 
     Form, FormGroup, Input, InputGroup,
-    InputGroupAddon, InputGroupText, Label, Modal, ModalBody, ModalFooter
+    InputGroupAddon, InputGroupText, Label
  } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -20,7 +20,6 @@ const HomePageBookTable = () => {
     })
     const {fullname, email, phone, date, guests, time} = value
     const [isChecked, setIsChecked] = useState(false)
-    const [modal, setModal] = useState(false)
     const [divSlideIn, setDivSlideIn ] = useState(false)
     const [formDiv, setFormDiv] = useState(true)
 
@@ -45,9 +44,6 @@ const HomePageBookTable = () => {
         setValue({ ...value, [e.target.name]: e.target.value})  
     }
 
-    const toggleModal = () => {
-        setModal(!modal)
-    }
 
     return(
         <Row className="reserved-table-section">
@@ -152,110 +148,112 @@ const HomePageBookTable = () => {
                     </Col>
                     <Col xs="12" sm="6" className="d-none d-md-block">
                         <img src={WaiterServing} alt="waiter-serving" />
-                    </Col>
+                    </Col> 
                 </>
                     :
-                <>
-                    <Col xs="12" sm="6" className="mb-4 form-hide-and-show">
-                        <Form id="form-section" onSubmit={handleSubmit}>
-                            <Row form>
-                                <Col xs="12">
-                                    <h4>
-                                        Reserve A Table
-                                    </h4>
-                                </Col>
-                                <Col xs="12" sm="6" className="mb-4">
-                                    <InputGroup>
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <FontAwesomeIcon icon={['far', 'user']} />
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input type="text" value={fullname} name="fullname" onChange={handleChange} placeholder="FullName" />
-                                    </InputGroup>
-                                    {/* <FormText>
-                                        Enter Firstname and Lastname
-                                    </FormText> */}
-                                </Col>
-                                <Col xs="12" sm="6" className="mb-4">
-                                    <InputGroup>
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <FontAwesomeIcon icon={['far', 'envelope']} />
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input type="email" value={email} name="email" onChange={handleChange}  placeholder="Email" />
-                                    </InputGroup>
-                                </Col>
-                                <Col xs="12" sm="6" className="mb-4">
-                                    <InputGroup>
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <FontAwesomeIcon icon="mobile-alt" />
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input type="number" value={phone} name="phone" onChange={handleChange}
-                                        placeholder="Phone no"
-                                        maxLength="11"
-                                        />
-                                    </InputGroup>
-                                </Col>
-                                <Col xs="12" sm="6" className="mb-4">
-                                    <InputGroup>
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <FontAwesomeIcon icon={['far', 'calendar-alt']} />
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input type="date" value={date} name="date" onChange={handleChange} placeholder="Check-in-Date" />
-                                    </InputGroup>
-                                </Col>
-                                <Col xs="12" sm="6" className="mb-4">
-                                    <InputGroup>
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <FontAwesomeIcon icon="users" />
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input type="select" value={guests} name="guests" onChange={handleChange} placeholder="No of Guests"> 
-                                            <option value="choose">No of guests </option>
-                                            <option value="2">2</option>
-                                            <option value="5">5</option>
-                                            <option value="8">8</option>
-                                            <option value="10">10</option>
-                                        </Input>
-                                    </InputGroup>
-                                </Col>
-                                <Col xs="12" sm="6" className="mb-4">
-                                    <InputGroup>
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <FontAwesomeIcon icon={['far', 'clock']} />
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input type="select" value={time} name="time" onChange={handleChange} placeholder="Time"> 
-                                            <option value="time">Choose a time</option>
-                                            <option value="9AM TO 11AM">9AM TO 11AM</option>
-                                            <option value="11:30AM TO 12:30PM">11:30AM TO 12:30PM</option>
-                                            <option value="3PM TO 5PM">3PM TO 5PM</option>
-                                            <option value="6PM TO 8PM">6PM TO 8PM</option>
-                                        </Input>
-                                    </InputGroup>
-                                </Col>
-                            </Row>
-                            <FormGroup check>
-                                <Input type="checkbox" checked={isChecked} onChange={handleChange} name="reserve" className="my-2" />
-                                <Label for="reserve" check className="reserve-label"> 
-                                    Send reservation details to my email
-                                </Label>
-                            </FormGroup>
-                            <Button className="my-3 booking">Reserve A Table Now</Button>
-                        </Form>
-                    </Col>
-                    <Col xs="12" sm="6" className="d-none d-md-block">
-                        <img src={WaiterServing} alt="waiter-serving" />
-                    </Col>
-                </> 
+                <Col className="form-hide-and-show">
+                    <Row>
+                        <Col xs="12" sm="6" className="mb-4 form-hide-and-show">
+                            <Form id="form-section" onSubmit={handleSubmit}>
+                                <Row form>
+                                    <Col xs="12">
+                                        <h4>
+                                            Reserve A Table
+                                        </h4>
+                                    </Col>
+                                    <Col xs="12" sm="6" className="mb-4">
+                                        <InputGroup>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <FontAwesomeIcon icon={['far', 'user']} />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="text" value={fullname} name="fullname" onChange={handleChange} placeholder="FullName" />
+                                        </InputGroup>
+                                        {/* <FormText>
+                                            Enter Firstname and Lastname
+                                        </FormText> */}
+                                    </Col>
+                                    <Col xs="12" sm="6" className="mb-4">
+                                        <InputGroup>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <FontAwesomeIcon icon={['far', 'envelope']} />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="email" value={email} name="email" onChange={handleChange}  placeholder="Email" />
+                                        </InputGroup>
+                                    </Col>
+                                    <Col xs="12" sm="6" className="mb-4">
+                                        <InputGroup>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <FontAwesomeIcon icon="mobile-alt" />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="number" value={phone} name="phone" onChange={handleChange}
+                                            placeholder="Phone no"
+                                            maxLength="11"
+                                            />
+                                        </InputGroup>
+                                    </Col>
+                                    <Col xs="12" sm="6" className="mb-4">
+                                        <InputGroup>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <FontAwesomeIcon icon={['far', 'calendar-alt']} />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="date" value={date} name="date" onChange={handleChange} placeholder="Check-in-Date" />
+                                        </InputGroup>
+                                    </Col>
+                                    <Col xs="12" sm="6" className="mb-4">
+                                        <InputGroup>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <FontAwesomeIcon icon="users" />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="select" value={guests} name="guests" onChange={handleChange} placeholder="No of Guests"> 
+                                                <option value="choose">No of guests </option>
+                                                <option value="2">2</option>
+                                                <option value="5">5</option>
+                                                <option value="8">8</option>
+                                                <option value="10">10</option>
+                                            </Input>
+                                        </InputGroup>
+                                    </Col>
+                                    <Col xs="12" sm="6" className="mb-4">
+                                        <InputGroup>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                    <FontAwesomeIcon icon={['far', 'clock']} />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input type="select" value={time} name="time" onChange={handleChange} placeholder="Time"> 
+                                                <option value="time">Choose a time</option>
+                                                <option value="9AM TO 11AM">9AM TO 11AM</option>
+                                                <option value="11:30AM TO 12:30PM">11:30AM TO 12:30PM</option>
+                                                <option value="3PM TO 5PM">3PM TO 5PM</option>
+                                                <option value="6PM TO 8PM">6PM TO 8PM</option>
+                                            </Input>
+                                        </InputGroup>
+                                    </Col>
+                                </Row>
+                                <FormGroup check>
+                                    <Input type="checkbox" checked={isChecked} onChange={handleChange} name="reserve" className="my-2" />
+                                    <Label for="reserve" check className="reserve-label"> 
+                                        Send reservation details to my email
+                                    </Label>
+                                </FormGroup>
+                                <Button className="my-3 booking">Reserve A Table Now</Button>
+                            </Form>
+                        </Col>
+                        <Col xs="12" sm="6" className="d-none d-md-block">
+                            <img src={WaiterServing} alt="waiter-serving" />
+                        </Col>
+                    </Row>
+                </Col> 
             }
             
 
